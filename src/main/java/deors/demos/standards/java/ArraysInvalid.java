2993af6f621983d0f28add2709a26e3dc4ae4ceb
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2015-08-04 14:06:40 +0200

    added two classes to test rule coverage for Array declaration conventions

---
+++ src/main/java/deors/demos/standards/java/ArraysInvalid.java
@@ -0,0 +1,33 @@
+package deors.demos.standards.java;
+
+/**
+ * Class showing invalid array declarations according to coding standards.
+ *
+ * @author jorge.hidalgo
+ * @version 1.0
+ */
+public class ArraysInvalid {
+
+    /**
+     * An invalid one-line array declaration - brackets position.
+     */
+    int list1[] = {0, 1, 2, 3, 4, 5};
+
+    /**
+     * An invalid one-line array declaration - extra whitespaces.
+     */
+    int[] list2 = { 0, 1, 2, 3, 4, 5 };
+
+    /**
+     * An invalid multi-line array declaration - brackets position.
+     */
+    int[] list3 = {
+        0, 1, 2, 3, 4, 5
+        };
+
+    /**
+     * An invalid multi-line array declaration - brackets position.
+     */
+    int[] list4 = {
+        0, 1, 2, 3, 4, 5};
+}
---
+++ src/main/java/deors/demos/standards/java/ArraysValid.java
@@ -0,0 +1,30 @@
+package deors.demos.standards.java;
+
+/**
+ * Class showing valid array declarations according to coding standards.
+ *
+ * @author jorge.hidalgo
+ * @version 1.0
+ */
+public class ArraysValid {
+
+    /**
+     * A valid one-line array declaration.
+     */
+    int[] list1 = {0, 1, 2, 3, 4, 5};
+
+    /**
+     * A valid multi-line array declaration.
+     */
+    int[] list2 = {
+        0, 1, 2, 3, 4, 5
+    };
+
+    /**
+     * Default constructor.
+     */
+    public ArraysValid() {
+
+        super();
+    }
+}
